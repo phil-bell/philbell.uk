b3c95af32c9f2bf210a4ecd2e93838b645a5cd8f
Phil <philhabell@gmail.com>
2020-05-02 22:21:41 +0100

    Building start of frontend
    - gitignore changes
    - dockerignore changes
    - adding node-sass
    - adding index.html back
    - removing css
    - adding App, Container and Sidebar components scss
    - adding index.scss
    - Adding Sidebar and Container components to App
    - Adding Container component
    - Adding Sidebar Component
    - Removing node_modules/temp

--- src/client/src/components/App.js
+++ src/client/src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import '../assets/scss/components/App.scss';
+import Sidebar from './Sidebar';
+import Container from './Container';
 
 function App() {
 
   return (
-    <div className="App">
-      Hello there!
+    <div className="app">
+      <Sidebar></Sidebar>
+      <div className="app__page">
+          <Container></Container>
+      </div>
     </div>
   );
 }
