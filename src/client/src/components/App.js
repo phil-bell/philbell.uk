765a8391360fc512d2d2251129f64897cd31d32e
philhabell <philhabell@gmail.com>
2020-05-03 21:40:43 +0100

    Starting page to add page navigation: - Adding react-router-dom to package - Adding Router and Routes to App - Adding Home component with scss (empty)
    NOTE: This code is not currently functional!

--- src/client/src/components/App.js
+++ src/client/src/components/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import '../assets/scss/components/App.scss';
 import Sidebar from './Sidebar';
 import Container from './Container';
+import Home from './Home';
+import Admin from './Admin';
+import Contact from './Contact';
+import Login from './Login';
+import Resume from './Resume';
+
 
 function App() {
 
   return (
-    <div className="app">
-      <Sidebar></Sidebar>
-      <div className="app__page">
-          <Container></Container>
+    <Router>
+      <div className="app">
+        <Sidebar></Sidebar>
+        <div className="app__page">
+          <Route exact path="/" component={Home} />
+          <Route path="/admin" component={Admin} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/login" component={Login} />
+          <Route path="/resume" component={Resume} />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
