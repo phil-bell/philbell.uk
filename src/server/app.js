464541d4d0ca40d9e27699f6852d90459784a3d2
philhabell <philhabell@gmail.com>
2020-05-23 19:55:52 +0100

    Fixing connection to docker mongo container

--- src/server/app.js
+++ src/server/app.js
@@ -8,14 +8,12 @@ var indexRouter = require('./routes/index');
 var testRouter = require('./routes/test');
 const User = require('./routes/users');
 const mongoose = require('mongoose');
-const mongo_uri = 'mongodb://localhost:27017/db';
-mongoose.connect(mongo_uri, function(err) {
-  if (err) {
-    throw err;
-  } else {
-    console.log(`Successfully connected to ${mongo_uri}`);
-  }
-});
+try {
+  mongoose.connect('mongodb://db:27017/db', { useNewUrlParser: true });
+  console.log("Successfully connnected to mongo server...")
+} catch (error) {
+  handleError(error);
+}
 
 var app = express();
 
