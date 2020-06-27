10c572e2d38cbfff62325cfcd6ef8ae6c926f74f
philhabell <philhabell@gmail.com>
2020-06-27 23:53:14 +0100

    more backend login changes and starting frontend login

--- src/client/src/components/Login.js
+++ src/client/src/components/Login.js
@@ -1,18 +1,44 @@
-import React from 'react';
-import '../assets/scss/components/Login.scss';
+import React from "react";
+import axios from "axios"
+import "../assets/scss/components/Login.scss";
 
-function Login() {
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ username: event.target.username, password: event.target.password });
+  }
 
+  handleSubmit(event) {
+    event.preventDefault()
+    axios
+      .post("localhost:9000/login", {
+        username: this.state.username,
+        password: this.state.passowrd,
+      })
+      .then(function (response) {
+        alert(response)
+      });
+  }
+
+  render() {
     return (
-        <div className="login">
-            <div className="app__content">
-            <form action="/api/login" method="post">
-                <input id="username" name="username" placeholder="username"></input>
-                <input id="password" name="password" placeholder="password"></input>
-                <button type="submit">login</button>
-            </form>
-            </div>
+      <div className="login">
+        <div className="app__content">
+          <form onSubmit={this.handleSubmit}>
+            <input value={this.state.username} onChange={this.handleChange}></input>
+            <input value={this.state.password} onChange={this.handleChange}></input>
+            <button type="submit">login</button>
+          </form>
         </div>
-    )
+      </div>
+    );
+  }
 }
-export default Login;
--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const app = express();
+const cors = require('cors')
+const db = require("./routes/db");
 const PORT = 9000;
-let users = { phil: "pass" };
+const app = express();
+const mongoose = db.conn();
+const admin = db.create_admin();
 
+const Users = db.Users;
+
+app.use(cors())
 app.use(express.urlencoded());
 app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to my API");
@@ -13,20 +20,37 @@ app.get("/", (req, res) => {
 app.post("/register", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  users[username] = password;
-  res.send("user created");
+  const newUser = new Users({ username: username, password: password, perms: "user"});
+  res.send(`Successfully created user: ${newUser.username}`);
 });
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  if (username && password && users[username] == password) {
-    res.send("credentials correct");
-  } else {
-    res.send("incorrect credentials");
-  }
+  Users.find({ username: username, password: password }, (err, user) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(user.length > 0);
+    }
+  });
 });
 
-app.listen(PORT, function () {
+app.get("/all_users", (req, res) => {
+  Users.find({}, (err, users) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(users);
+    }
+  });
+});
+
+app.post("/clear_database", (req, res) => {
+  db.clr_db();
+  res.send("Database cleared");
+});
+
+app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
