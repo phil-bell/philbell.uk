aa42999f88981c235c029c47f5ab956fdde52333
Philip Bell <philhabell@gmail.com>
2020-10-24 20:38:01 +0100

    changing postgres creds

--- settings/linux.py
+++ settings/linux.py
@@ -1,8 +1,8 @@
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "philipbell",
-        "USER": "philipbell",
+        "NAME": "postgres",
+        "USER": "postgres",
         "PASSWORD": "postgres",
         "HOST": "localhost",
         "PORT": "5433",
