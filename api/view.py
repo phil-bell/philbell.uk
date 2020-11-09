75fb023b65c1b78c5248971883f11b2ea5a6673a
philhabell <phil@Philips-PC.localdomain>
2020-11-09 00:20:01 +0000

    adding input component and making login form better

--- api/view.py
+++ api/view.py
@@ -5,7 +5,6 @@ from rest_framework import mixins
 from rest_framework.views import APIView
 from django.contrib.auth.models import User, Group
 from django.http.response import JsonResponse
-from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.response import Response
 from subprocess import run
