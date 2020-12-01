594410fe0ae4a6fbed1e6e1937db2e4304ee0629
philhabell <philhabell@gmail.com>
2020-12-01 23:27:42 +0000

    updating start script

--- scripts.py
+++ scripts.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python
-from subprocess import run
+from subprocess import run as _run()
 from threading import Thread
 
+def run(command):
+    _run(commmand.split())
 
 def postgresql():
-    run("sudo service postgresql start".split())
+    run("sudo service postgresql start")
 
 
 def qbittorrent():
-    run("qbittorrent-nox".split())
+    run("qbittorrent-nox")
 
 
 def django():
-    run("python manage.py runserver".split())
+    run("python manage.py runserver")
 
 
 def django_prod():
-    run("python manage.py runserver 0.0.0.0:8000".split())
+    run("uwsgi --http :8000 --module app.wsgi --enable-threads")
 
 
 def webpack():
-    run("npm run watch".split())
+    run("npm run watch")
 
 
 def migrate():
-    run("python manage.py makemigrations".split())
-    run("python manage.py migrate".split())
+    run("python manage.py makemigrations")
+    run("python manage.py migrate")
 
 
 def format():
-    run("black . --line-length 99".split())
-    run("npm run prettier".split())
+    run("black . --line-length 99")
+    run("npm run prettier")
 
 
 def start():
