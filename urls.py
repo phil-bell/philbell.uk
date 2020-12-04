39733ab0d34b331acae5e7977c3bb0e1d295e630
philhabell <philhabell@gmail.com>
2020-12-04 23:49:13 +0000

    moving up admin

--- urls.py
+++ urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include, re_path
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r"^\.well-known/", include("letsencrypt.urls")),
     path("admin/", admin.site.urls),
+    url(r"^\.well-known/", include("letsencrypt.urls")),
     path("api/", include("api.urls")),
     path("", include("app.urls")),
 ]
