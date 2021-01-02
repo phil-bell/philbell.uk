ef3361d303914d3f45a9318b0db5c01dcb5dfe97
philhabell <philhabell@gmail.com>
2021-01-02 14:55:47 +0000

    setting toast background and removing webkit styling

--- app/static-src/js/components/tables/add-row.js
+++ app/static-src/js/components/tables/add-row.js
@@ -42,8 +42,11 @@ export class AddRow extends BaseTableRow {
           outline: none;
         }
         select {
+          background: url(data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA0Ljk1IDEwIj48ZGVmcz48c3R5bGU+LmNscy0xe2ZpbGw6I2ZmZjt9LmNscy0ye2ZpbGw6IzQ0NDt9PC9zdHlsZT48L2RlZnM+PHRpdGxlPmFycm93czwvdGl0bGU+PHJlY3QgY2xhc3M9ImNscy0xIiB3aWR0aD0iNC45NSIgaGVpZ2h0PSIxMCIvPjxwb2x5Z29uIGNsYXNzPSJjbHMtMiIgcG9pbnRzPSIxLjQxIDQuNjcgMi40OCAzLjE4IDMuNTQgNC42NyAxLjQxIDQuNjciLz48cG9seWdvbiBjbGFzcz0iY2xzLTIiIHBvaW50cz0iMy41NCA1LjMzIDIuNDggNi44MiAxLjQxIDUuMzMgMy41NCA1LjMzIi8+PC9zdmc+) no-repeat 95% 50%;
+          appearance: none;
+          -moz-appearance: none; 
+          -webkit-appearance: none; 
           color: var(--secondary-color);
-          background: var(--primary-color);
           border: 1px solid var(--secondary-color);
           border-radius: 5px;
           height: 25px;
@@ -51,6 +54,8 @@ export class AddRow extends BaseTableRow {
           -webkit-font-smoothing: antialiased;
           font-weight: 500;
           outline: none;
+          padding-left: 6px;
+          padding-right: 15px;
         }
       `,
     ];
