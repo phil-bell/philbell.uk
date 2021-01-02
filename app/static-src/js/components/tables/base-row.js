7b54d2748c01ddc3694cfa06146a48fbad146f04
philhabell <philhabell@gmail.com>
2021-01-02 00:35:04 +0000

    running autofixer on table css

--- app/static-src/js/components/tables/base-row.js
+++ app/static-src/js/components/tables/base-row.js
@@ -4,58 +4,73 @@ import Cookies from "js-cookie";
 export class BaseTableRow extends LitElement {
   static get styles() {
     return css`
-      :host {
-        display: flex;
-        flex-direction: column;
-      }
+    :host {
+      display: -webkit-box;
+      display: -ms-flexbox;
+      display: flex;
+      -webkit-box-orient: vertical;
+      -webkit-box-direction: normal;
+          -ms-flex-direction: column;
+              flex-direction: column;
+    }
 
-      .grid__row {
-        display: grid;
-        grid-auto-flow: row;
-        grid-template-columns: 1fr;
-        border-bottom: 1px solid var(--primary-color);
-        overflow: hidden;
-        transition: 0.2s ease all;
-        -moz-transition: 0.2s ease all;
-        -webkit-transition: 0.2s ease all;
-        height: 40px;
-      }
-      .grid__row__info {
-        display: grid;
-        grid-auto-flow: column;
-        grid-template-columns: 1fr;
-        height: 40px;
-      }
-      .grid__row__form {
-        display: grid;
-        justify-items: end;
-        grid-auto-flow: column;
-        grid-template-columns: 1fr;
-        height: 40px;
-      }
+    .grid__row {
+      display: -ms-grid;
+      display: grid;
+      grid-auto-flow: row;
+      -ms-grid-columns: 1fr;
+      grid-template-columns: 1fr;
+      border-bottom: 1px solid var(--primary-color);
+      overflow: hidden;
+      -o-transition: 0.2s ease all;
+      transition: 0.2s ease all;
+      -moz-transition: 0.2s ease all;
+      -webkit-transition: 0.2s ease all;
+      height: 40px;
+    }
+    .grid__row__info {
+      display: -ms-grid;
+      display: grid;
+      grid-auto-flow: column;
+      -ms-grid-columns: 1fr;
+      grid-template-columns: 1fr;
+      height: 40px;
+    }
+    .grid__row__form {
+      display: -ms-grid;
+      display: grid;
+      justify-items: end;
+      grid-auto-flow: column;
+      -ms-grid-columns: 1fr;
+      grid-template-columns: 1fr;
+      height: 40px;
+    }
 
-      .grid__cell {
-        padding: 5px;
-        align-self: center;
-        overflow: hidden;
-        white-space: nowrap;
-        text-overflow: ellipsis;
-      }
-      [open] {
-        height: 80px;
-      }
-      .only-mobile {
+    .grid__cell {
+      padding: 5px;
+      -ms-flex-item-align: center;
+          -ms-grid-row-align: center;
+          align-self: center;
+      overflow: hidden;
+      white-space: nowrap;
+      -o-text-overflow: ellipsis;
+         text-overflow: ellipsis;
+    }
+    [open] {
+      height: 80px;
+    }
+    .only-mobile {
+      display: none;
+    }
+
+    @media only screen and (max-width: 725px) {
+      .no-mobile {
         display: none;
       }
-
-      @media only screen and (max-width: 725px) {
-        .no-mobile {
-          display: none;
-        }
-        .only-mobile {
-          display: block;
-        }
+      .only-mobile {
+        display: block;
       }
+    }
     `;
   }
 
